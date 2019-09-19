28ed07220b7244fd396b2f56a1a06dfc9ffe7775
Foxnova123 <52623031+Foxnova123@users.noreply.github.com>
2019-09-20 00:29:41 +0700

    Update

--- PolymorphismLab/Program.cs
+++ PolymorphismLab/Program.cs
@@ -11,7 +11,10 @@ namespace Operations
     {
         static void Main(string[] args)
         {
-
+            MathOperations mo = new MathOperations();
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
             Animal cat = new Cat("Pesho", "Whiskas");
             Animal dog = new Dog("Gosho", "Meat");
 
@@ -87,6 +90,88 @@ namespace Animals
                 .Append("DJAAF");
 
             return builder.ToString();
+        }
+    }
+}
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return "Drawing ";
+        }
+        public class Circle : Shape
+        {
+            private double radius;
+
+            public Circle(double radius)
+            {
+                Radius = radius;
+            }
+
+            public double Radius
+            {
+                get { return radius; }
+                private set { radius = value; }
+            }
+
+            public override double CalculateArea()
+            {
+                return Math.PI * Math.Pow(Radius, 2);
+            }
+
+            public override double CalculatePerimeter()
+            {
+                return 2 * Math.PI * Radius;
+            }
+
+            public override string Draw()
+            {
+                return base.Draw() + this.GetType().Name;
+            }
+        }
+        public class Rectangle : Shape
+        {
+            private double width;
+            private double height;
+
+            public Rectangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public double Width
+            {
+                get { return width; }
+                private set { width = value; }
+            }
+
+            public double Height
+            {
+                get { return height; }
+                private set { height = value; }
+            }
+
+            public override double CalculateArea()
+            {
+                return Width * Height;
+            }
+
+            public override double CalculatePerimeter()
+            {
+                return 2 * Width + 2 * Height;
+            }
+
+            public override string Draw()
+            {
+                return base.Draw() + this.GetType().Name;
+            }
         }
     }
 }

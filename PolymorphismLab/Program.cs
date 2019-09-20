43c7fba439df92050543e96179ce8bc46657d970
Foxnova123 <52623031+Foxnova123@users.noreply.github.com>
2019-09-20 10:12:54 +0700

    +Add code

--- PolymorphismLab/Program.cs
+++ PolymorphismLab/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Animals;
+using Shapes;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Shapes.Shape;
 
 namespace Operations
 {
@@ -11,16 +13,22 @@ namespace Operations
     {
         static void Main(string[] args)
         {
-            MathOperations mo = new MathOperations();
-            Console.WriteLine(mo.Add(2, 3));
-            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
-            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
-            Animal cat = new Cat("Pesho", "Whiskas");
-            Animal dog = new Dog("Gosho", "Meat");
+            // MathOperations mo = new MathOperations();
+            //Console.WriteLine(mo.Add(2, 3));
+            // Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            //Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+            //Animal cat = new Cat("Pesho", "Whiskas");
+            //Animal dog = new Dog("Gosho", "Meat");
 
-            Console.WriteLine(cat.ExplainSelf());
-            Console.WriteLine(dog.ExplainSelf());
+            //Console.WriteLine(cat.ExplainSelf());
+            //Console.WriteLine(dog.ExplainSelf());
+            Shape circle = new Circle(5);
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
 
+            Shape rectangle = new Rectangle(5,5);
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
             Console.ReadLine();
         }
     }
